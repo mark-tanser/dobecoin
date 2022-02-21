a9ad64e39a01fbd3228549e375690dd504e7375b
Mark Tanser <tanser.mark@gmail.com>
2022-02-21 16:23:58 +0900

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
-const REDIS_PROD_URL = 'input your redi url here'
-const WEBSITE_URL = 'inout your deployed website url here';
+const REDIS_PROD_URL = 'input your redis url here'
+const WEBSITE_URL = 'input your deployed website url here';
 module.exports = { 
     REDIS_PROD_URL,
     WEBSITE_URL
